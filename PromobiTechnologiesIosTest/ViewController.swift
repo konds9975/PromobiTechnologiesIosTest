2561a10aec14b2ea0b4ec079d29690733f4c0d65
kondya <konds9975@gmail.com>
2019-07-02 09:41:12 +0530

    Initial Commit

---
+++ PromobiTechnologiesIosTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PromobiTechnologiesIosTest
+//
+//  Created by Kondya on 02/07/19.
+//  Copyright © 2019 Fortune4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PromobiTechnologiesIosTestTests/PromobiTechnologiesIosTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  PromobiTechnologiesIosTestTests.swift
+//  PromobiTechnologiesIosTestTests
+//
+//  Created by Kondya on 02/07/19.
+//  Copyright © 2019 Fortune4. All rights reserved.
+//
+
+import XCTest
+@testable import PromobiTechnologiesIosTest
+
+class PromobiTechnologiesIosTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
